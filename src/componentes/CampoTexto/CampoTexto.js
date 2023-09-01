5b44a8b761bde5129140925533301fac449cfbb5
Eduardo Bueno <lcdoandresbuenoadm@gmail.com>
2023-09-01 19:05:57 -0400

    version5

---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,21 @@
+//import { useState } from "react"
+import "./Campotexto.css"
+const Campotexto = (props) => {
+    const placeholderModificado = `${props.placeholder}...`
+
+    const manejarCambio = (e) =>{
+        
+        props.actualizarValor(e.target.value)
+    }
+    return <div className="campo-texto">
+        <label>{props.titulo}</label>
+        <input
+             placeholder={placeholderModificado}
+             required={props.required} 
+             value= {props.valor}
+             onChange={manejarCambio}
+    />
+    </div>
+}
+
+export default Campotexto
